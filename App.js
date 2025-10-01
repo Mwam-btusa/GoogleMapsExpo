baaed897491c881c751114463a0f5adc18a984f2
MWAMBTUSA ANDRE MACEDO JACINTO <mwambtusa.jacinto@etec.sp.gov.br>
2025-09-30 21:04:06 -0300

    atividade com google maps

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={ styles.mapa}
+        initialRegion={{
+        latitude: -23.55052,
+        longitude: -46.633308,
+        latitudeDelta: 0.0922,
+        longitudeDelta:0.0421,
+        }}
+        >
+        <marker
+          coordinate={{latitude: -23.55052, longitude: -46.633308,}}
+          title={"São Paulo"}
+          description={"Capital financeira do Brasil"}
+        />
+      </MapView>
     </View>
   );
 }
@@ -14,8 +27,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  mapa: {
+    width: Dimensions.get('window').width,
+    height:Dimensions.get('window').height,
+  },  
 });
